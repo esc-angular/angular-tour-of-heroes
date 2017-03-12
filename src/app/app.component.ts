70efedd82efcd19b5035043ce92025943be5dc20
HuyDD <dod.huy@gmail.com>
2017-03-12 19:26:46 +0700

    Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }; F
+  heroes: Hero[] = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
-  id: number;
-  name: string;
+  constructor(public id: number,
+              public name: string) {};
 }
 
+const HEROES: Hero[] = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(13, 'Narco'),
+  new Hero(14, 'Bombasto'),
+  new Hero(15, 'Celeritas'),
+  { 'id': 16, 'name': 'Magneta'},
+  { 'id': 17, 'name': 'RubberMan'},
+  { 'id': 18, 'name': 'Dr IQ'},
+  { 'id': 19, 'name': 'Magma'},
+  { 'id': 20, 'name': 'Tornado'}
+];
